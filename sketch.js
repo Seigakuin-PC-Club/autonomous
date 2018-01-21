037893a492611751ab8ccec0da6a91a418879d61
S Fujimoto <unicoshun@gmail.com>
2018-01-21 10:52:30 +0900

    Add Japanese comments to code / Add debug on off checkbox

--- sketch.js
+++ sketch.js
@@ -3,9 +3,10 @@
 var vehicles = [];
 var food = [];
 var poison = [];
+var debug;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight - 20);
 
   // create vehicles on canvas
   for (var i = 0; i < 10; i++) {
@@ -27,48 +28,59 @@ function setup() {
     var y = random(height);
     poison.push(createVector(x, y));
   }
+  debug = createCheckbox();
 }
 
+// add more vehicles on canvas
 function mouseDragged() {
   vehicles.push(new Vehicle(mouseX, mouseY));
 }
-
+/**
+ * DRAW FUNCTION
+ **/
 function draw() {
   background(51);
   fill(255);
 
+  // add food randomly on canvas
   if (random(1) < 0.1) {
+    // フレームごとに乱数を生成、0.1以下だったらfood生成
     var x = random(width);
     var y = random(height);
     food.push(createVector(x, y));
   }
+  // フレームごとに乱数を生成、0.1以下だったらpoison生成
   if (random(1) < 0.01) {
     var x = random(width);
     var y = random(height);
     poison.push(createVector(x, y));
   }
 
+  // foodをcanvasに描く
   for (let i = 0; i < food.length; i++) {
     fill(0, 255, 0);
     ellipse(food[i].x, food[i].y, 8, 8);
   }
+  // poisonをcanvasに描く
   for (let i = 0; i < poison.length; i++) {
     fill(255, 0, 0);
     ellipse(poison[i].x, poison[i].y, 8, 8);
   }
+
+  // forを逆ループにするのは、dead時にspliceするときに起こるエラーを回避するため
   for (var i = vehicles.length - 1; i >= 0; i--) {
     vehicles[i].boundaries();
-    vehicles[i].behaviors(food, poison);
-    // Call the appropriate steering behaviors for our agents
-    // vehicle.seek(food);
+    vehicles[i].behaviors(food, poison); // eatした時の行動
     vehicles[i].update();
     vehicles[i].display();
 
+    // 条件が合えばcloneを生成
     var newVehicle = vehicles[i].clone();
     if (newVehicle != null) {
       vehicles.push(newVehicle);
     }
 
+    // deadだったらspliceする
     if (vehicles[i].dead()) {
       var x = vehicles[i].position.x;
       var y = vehicles[i].position.y;
--- vehicle.js
+++ vehicle.js
@@ -1,26 +1,32 @@
-var mr = 0.01;
+var mr = 0.01; // mutation rate - 1%の確率でdnaを変化
 
 function Vehicle(x, y, dna) {
-  this.acceleration = createVector(0, 0);
-  this.velocity = createVector(0, -2);
-  this.position = createVector(x, y);
-  this.r = 4;
-  this.maxspeed = 5;
-  this.maxforce = 0.5;
+  this.acceleration = createVector(0, 0); // 加速
+  this.velocity = createVector(0, -2); // 速さ
+  this.position = createVector(x, y); // 場所
+  this.r = 6; // vehicleの大きさ
+  this.maxspeed = 3;
+  this.maxforce = 0.3;
 
-  this.health = 1;
+  this.health = 1; // 体力(health)の初期値　フレームごとに減衰
 
+  //各vehicleが持っているDNA（性質）をArrayに設定
   this.dna = [];
+  // dna=undefined - new Vehicle時にdnaが渡されなかったら
+  // dna[]の初期値を設定
   if (dna === undefined) {
-    // food weight
+    // food weight - foodに向かうベクトル(steerG)強さ
     this.dna[0] = random(-2, 2);
-    // poison weight
+    // poison weight - poisionに向かうベクトル(steerB)強さ
     this.dna[1] = random(-2, 2);
-    // food perception
+    // food perception - foodが見える範囲(視界)
     this.dna[2] = random(0, 100);
-    // poison perception
+    // poison perception - perceptionが見える範囲(視界)
     this.dna[3] = random(0, 100);
   } else {
+    // すでにdnaを持っている(cloneされた new Vehicle)場合
+    // 各dna値をコピーする
+    // その際、mr(mutation rate)に応じた突然変異を起こす
     this.dna[0] = dna[0];
     if (random(1) < mr) {
       this.dna[0] += random(-0.1, 0.1);
@@ -44,7 +50,7 @@ function Vehicle(x, y, dna) {
    **/
   // Method to update location
   this.update = function() {
-    this.health -= 0.01;
+    this.health -= 0.01; // フレームごとに0.01 healthが減衰
 
     // Update velocity
     this.velocity.add(this.acceleration);
@@ -57,10 +63,15 @@ function Vehicle(x, y, dna) {
 
   /**
    * BEHAVIORS Func
+   * vehicleがeatした時のbehavior(action)を設定
    **/
   this.behaviors = function(good, bad) {
+    // good = food Array / bad = poison Array
+    // 0.3 = nutrition(栄養)  - healthに足される nutritionの量
+    // dna[2] = food perception(food視界)をeat渡す
     var steerG = this.eat(good, 0.3, this.dna[2]);
     var steerB = this.eat(bad, -0.7, this.dna[3]);
+
     steerG.mult(this.dna[0]);
     steerB.mult(this.dna[1]);
 
@@ -78,6 +89,7 @@ function Vehicle(x, y, dna) {
    **/
   this.clone = function() {
     if (random(1) < 0.005) {
+      // フレームごとに0.5%の確率でクローンを生成
       return new Vehicle(this.position.x, this.position.y, this.dna);
     } else {
       return null;
@@ -91,11 +103,13 @@ function Vehicle(x, y, dna) {
     var record = Infinity;
     var closest = null;
     for (var i = list.length - 1; i >= 0; i--) {
+      // d = vehicleからfood/positionの距離(distance)
       var d = this.position.dist(list[i]);
 
+      // d(distance)がmaxspeed以下(エラー回避のため)
       if (d < this.maxspeed) {
-        list.splice(i, 1);
-        this.health += nutrition;
+        list.splice(i, 1); // food/poisonを消す(eatする)
+        this.health += nutrition; // food/poison値がhealthの増減に影響
       } else {
         if (d < record && d < perception) {
           record = d;
@@ -105,7 +119,6 @@ function Vehicle(x, y, dna) {
     }
 
     // This is the moment of eating
-
     if (closest != null) {
       return this.seek(closest);
     }
@@ -140,25 +153,31 @@ function Vehicle(x, y, dna) {
    * DISPLAY FUNC
    **/
   this.display = function() {
-    // Draw a triangle rotated in the direction of velocity
-    var theta = this.velocity.heading() + PI / 2;
+    // 三角形を描画
+    var theta = this.velocity.heading() + PI / 2; // 三角形の向きを調整
 
     push();
     translate(this.position.x, this.position.y);
     rotate(theta);
 
-    strokeWeight(3);
-    stroke(0, 255, 0);
-    noFill();
-    line(0, 0, 0, -this.dna[0] * 25);
-    ellipse(0, 0, this.dna[2] * 2, this.dna[2] * 2);
-    strokeWeight(2);
-    stroke(255, 0, 0);
-    line(0, 0, 0, -this.dna[1] * 25);
-    ellipse(0, 0, this.dna[3] * 2, this.dna[3] * 2);
+    if (debug.checked()) {
+      // 緑の線(poison weight)を引く
+      strokeWeight(3);
+      stroke(0, 255, 0); //緑
+      noFill();
+      line(0, 0, 0, -this.dna[0] * 25); //線を引く
+      ellipse(0, 0, this.dna[2] * 2, this.dna[2] * 2); // perspective(視界)の丸を描く
+      // 赤い線(food weight)を引く
+      strokeWeight(2);
+      stroke(255, 0, 0); //赤
+      line(0, 0, 0, -this.dna[1] * 25);
+      ellipse(0, 0, this.dna[3] * 2, this.dna[3] * 2); // perspective(視界)の丸を描く
+    }
 
-    var gr = color(0, 255, 0);
-    var rd = color(255, 0, 0);
+    // health値によってvehicleの色設定
+    var gr = color(0, 255, 0); //緑
+    var rd = color(255, 0, 0); // 赤
+    //lerpColorは rd から gr に this.healthの値によってmixする
     var col = lerpColor(rd, gr, this.health);
 
     fill(col);
@@ -175,23 +194,29 @@ function Vehicle(x, y, dna) {
 
   /**
    * BOUNDARIES FUNC
+   * 画面外に出てしまったときの対処
    **/
   this.boundaries = function() {
-    var desired = null;
-    var d = 25;
+    var desired = null; // 壁を避ける向きのベクトル(強さ)
+    var d = 25; // d = edgeからのdistance
 
     if (this.position.x < d) {
+      // もし左端に近づいたら
       desired = createVector(this.maxspeed, this.velocity.y);
     } else if (this.position.x > width - d) {
+      //もし右端に近づいたら
       desired = createVector(-this.maxspeed, this.velocity.y);
     }
 
     if (this.position.y < d) {
+      // もし画面上に近づいたら
       desired = createVector(this.velocity.x, this.maxspeed);
     } else if (this.position.y > height - d) {
+      // もし画面下に近づいたら
       desired = createVector(this.velocity.x, -this.maxspeed);
     }
 
+    // 上記の条件(画面端)にいた場合
     if (desired !== null) {
       desired.normalize();
       desired.mult(this.maxspeed);
